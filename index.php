63ed31cadc9dc433f5a37c822587a595eb8793b9
jivanrod <jivanrod@gmail.com>
2025-01-12 18:27:03 +0100

    fix

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (filter_has_var(INPUT_POST, 'crear_contacto')) {
             $agenda[ucwords(strtolower($nombre))] = $telefono;
         }
     }
-} else if (filter_has_var(INPUT_POST, 'limpiar')) {
+} else if (filter_has_var(INPUT_GET, 'limpiar_contactos') || filter_has_var(INPUT_POST, 'limpiar_contactos')) {
     $agenda = [];
 }
 ?>
@@ -71,8 +71,11 @@ if (filter_has_var(INPUT_POST, 'crear_contacto')) {
             <?php if (!empty($agenda)): ?>
                 <fieldset>
                     <legend>Vaciar Agenda</legend>
-                <!--   <a class="submit red button" href="<?= "{$_SERVER['PHP_SELF']}?limpiar=1" ?>">Vaciar</a> -->
-                    <input type="submit" class="submit red" name="limpiar"  value="Vaciar">
+                 <input class="submit red" type="submit" value="Vaciar Agenda" name="limpiar_contactos">
+                            <!-- Otras maneras de enviar la petición al servidor 
+                            <input class="submit red" type="submit" formaction="<?= "{$_SERVER['PHP_SELF']}?limpiar_contactos" ?>"  value="Vaciar Agenda">
+                            <a href="<?= "{$_SERVER['PHP_SELF']}?limpiar_contactos" ?>"><input type="button" class="submit red" value="Vaciar Agenda"></a>
+                            <a class="submit red button" href="<?= "{$_SERVER['PHP_SELF']}?limpiar_contactos" ?>">Vaciar Agenda</a> -->
                 </fieldset>
             <?php endif ?>
         </form>
--- index_html.php
+++ index_html.php
@@ -33,8 +33,11 @@
             <!-- Si la agenda no está vacía -->
             <fieldset>
                 <legend>Vaciar Agenda</legend>
-            <!--   <a class="submit red button" href="<?= "{$_SERVER['PHP_SELF']}?limpiar=1" ?>">Vaciar</a> -->
-              <input type="submit" class="submit red" name="limpiar"  value="Vaciar">
+           <input class="submit red" type="submit" value="Vaciar Agenda" name="limpiar_contactos">
+                            <!-- Otras maneras de enviar la petición al servidor 
+                            <input class="submit red" type="submit" formaction="<?= "{$_SERVER['PHP_SELF']}?limpiar_contactos" ?>"  value="Vaciar Agenda">
+                            <a href="<?= "{$_SERVER['PHP_SELF']}?limpiar_contactos" ?>"><input type="button" class="submit red" value="Vaciar Agenda"></a>
+                            <a class="submit red button" href="<?= "{$_SERVER['PHP_SELF']}?limpiar_contactos" ?>">Vaciar Agenda</a> -->
             </fieldset>
         </form>
     </body>
